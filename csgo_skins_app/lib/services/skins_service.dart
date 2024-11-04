83ad1d04a166a7a51a347c59fb0731e212d0a8cc
Matheus Baraldi <matheushbaraldi@gmail.com>
2024-11-04 18:21:15 -0300

    temp fix

--- csgo_skins_app/lib/services/skins_service.dart
+++ csgo_skins_app/lib/services/skins_service.dart
@@ -1,8 +1,15 @@
+import 'package:csgo_skins_app/domain/entities/skin.dart';
 import 'package:csgo_skins_app/services/abstract_service.dart';
 
 class SkinsService extends Service {
   @override
   String entity() {
     return 'skins';
+  }
+
+  @override
+  Future<List<Skin>> find() async {
+    final response = await super.find();
+    return response.map((data) => Skin.fromJson(data)).toList();
   }
 }
