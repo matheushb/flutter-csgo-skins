f7177f44942b8a078c6229e124e5ae0434f12817
Matheus Baraldi <matheushbaraldi@gmail.com>
2024-11-05 18:14:03 -0300

    feat: add balance on user

--- csgo_skins_app/lib/domain/entities/user.dart
+++ csgo_skins_app/lib/domain/entities/user.dart
@@ -3,23 +3,33 @@ class User {
   final String name;
   final String email;
   final String password;
+  double balance;
 
   User({
     required this.id,
     required this.name,
     required this.email,
     required this.password,
+    this.balance = 0.0,
   });
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
-        id: json['id'],
-        name: json['name'],
-        email: json['email'],
-        password: json['password']);
+      id: json['id'],
+      name: json['name'],
+      email: json['email'],
+      password: json['password'],
+      balance: json['balance']?.toDouble() ?? 0.0,
+    );
   }
 
   Map<String, dynamic> toJson() {
-    return {'id': id, 'name': name, 'email': email, 'password': password};
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'password': password,
+      'balance': balance,
+    };
   }
 }
