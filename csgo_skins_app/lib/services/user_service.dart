44b563b15b9660fac7b49a1dd1a489c78cefa70f
Matheus Baraldi <matheushbaraldi@gmail.com>
2024-11-05 18:13:31 -0300

    feat: remove implementation from abstract service

--- csgo_skins_app/lib/services/user_service.dart
+++ csgo_skins_app/lib/services/user_service.dart
@@ -1,47 +1,47 @@
 import 'dart:convert';
-import 'package:csgo_skins_app/domain/entities/skin.dart';
 import 'package:http/http.dart' as http;
+import 'package:csgo_skins_app/domain/entities/user.dart';
 import 'package:csgo_skins_app/services/abstract_service.dart';
 
-class SkinService extends Service<Skin> {
+class UserService extends Service<User> {
   final http.Client client;
 
-  String baseUrl = 'http://localhost:3000/skins';
+  String baseUrl = 'http://localhost:3000/users';
 
-  SkinService(this.client);
+  UserService(this.client);
 
   @override
-  Future<List<Skin>> find() async {
+  Future<List<User>> find() async {
     final response = await client.get(Uri.parse(baseUrl));
     final List<dynamic> jsonResponse = json.decode(response.body);
-    return jsonResponse.map((data) => Skin.fromJson(data)).toList();
+    return jsonResponse.map((data) => User.fromJson(data)).toList();
   }
 
   @override
-  Future<Skin> findOne(String id) async {
+  Future<User> findOne(String id) async {
     final response = await client.get(Uri.parse(baseUrl));
-    return Skin.fromJson(json.decode(response.body));
+    return User.fromJson(json.decode(response.body));
   }
 
   @override
-  Future<Skin> create(Object body) async {
+  Future<User> create(Object body) async {
     final response =
         await client.post(Uri.parse(baseUrl), body: jsonEncode(body));
-    return Skin.fromJson(json.decode(response.body));
+    return User.fromJson(json.decode(response.body));
   }
 
   @override
-  Future<Skin> update(String id, Object body) async {
+  Future<User> update(String id, Object body) async {
     final response =
         await client.patch(Uri.parse(baseUrl), body: jsonEncode(body));
-    return Skin.fromJson(json.decode(response.body));
+    return User.fromJson(json.decode(response.body));
   }
 
   @override
   Future<void> delete(String id) async {
     final response = await client.delete(Uri.parse(baseUrl));
     if (response.statusCode != 200) {
-      throw Exception('Failed to delete Skin');
+      throw Exception('Failed to delete user');
     }
   }
 }
