c7dadc787020a0a32ce793420d0596af4361c78a
Matheus Baraldi <matheushbaraldi@gmail.com>
2024-11-05 18:14:10 -0300

    feat: refactored method

--- csgo_skins_app/lib/components/skin_card.dart
+++ csgo_skins_app/lib/components/skin_card.dart
@@ -4,55 +4,75 @@ import 'package:flutter/material.dart';
 class SkinCard extends StatelessWidget {
   final Skin skin;
 
-  const SkinCard({required this.skin});
+  const SkinCard({super.key, required this.skin});
 
   @override
   Widget build(BuildContext context) {
     return Card(
+      color: Colors.grey[600],
       margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
       elevation: 4,
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12),
+        borderRadius: BorderRadius.circular(24),
+        side: const BorderSide(color: Color.fromARGB(255, 146, 98, 236)),
       ),
-      child: Column(
-        children: [
-          Image.network(
-            skin.imageUrl,
-            width: 150,
-            height: 150,
-            fit: BoxFit.cover,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  skin.name,
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  overflow: TextOverflow.ellipsis, // Adiciona o overflow
-                  maxLines: 1, // Limita a uma linha
+      child: SizedBox(
+        width: 150,
+        height: 150,
+        child: Column(
+          children: [
+            Expanded(
+              child: ClipRRect(
+                borderRadius:
+                    const BorderRadius.vertical(top: Radius.circular(24)),
+                child: Image.network(
+                  'https://cors-anywhere.herokuapp.com/${skin.imageUrl}',
+                  width: double.infinity,
+                  fit: BoxFit.contain,
+                  errorBuilder: (context, error, stackTrace) {
+                    return const Icon(Icons.broken_image, size: 75);
+                  },
                 ),
-                const SizedBox(height: 4),
-                Text(
-                  'Float: ${skin.float.toStringAsFixed(2)}',
-                  overflow: TextOverflow.ellipsis,
-                ),
-                Text(
-                  'R\$ ${skin.price.toStringAsFixed(2)}',
-                  style: const TextStyle(
-                    fontSize: 16,
-                    color: Colors.green,
-                  ),
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ],
+              ),
             ),
-          ),
-        ],
+            ClipRRect(
+              borderRadius:
+                  const BorderRadius.vertical(bottom: Radius.circular(24)),
+              child: Container(
+                padding: const EdgeInsets.all(8.0),
+                color: const Color.fromARGB(255, 167, 130, 236),
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      skin.name,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      skin.getWear(),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Text(
+                      'R\$ ${skin.price.toStringAsFixed(2)}',
+                      style: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
