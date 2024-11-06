c5a90bd0de6a67d32eb72dd9363c141cc772cfda
Matheus Baraldi <matheushbaraldi@gmail.com>
2024-11-06 18:10:36 -0300

    feat: implemented cart

--- csgo_skins_app/lib/components/cart.dart
+++ csgo_skins_app/lib/components/cart.dart
@@ -1,7 +1,22 @@
+import 'package:csgo_skins_app/domain/entities/cart.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class Cart extends StatelessWidget {
-  const Cart({super.key});
+class CartDrawer extends StatefulWidget {
+  const CartDrawer({super.key});
+
+  @override
+  _CartDrawerState createState() => _CartDrawerState();
+}
+
+class _CartDrawerState extends State<CartDrawer> {
+  late Cart cart;
+
+  @override
+  void initState() {
+    super.initState();
+    cart = Cart();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,20 +24,63 @@ class Cart extends StatelessWidget {
       backgroundColor: const Color.fromARGB(255, 20, 20, 20),
       child: ListView(
         children: [
-          ListTile(
-            title: const Text('Item 1'),
-            textColor: Colors.white,
-            onTap: () {
-              print('Item 1 clicado');
-            },
+          const ListTile(
+            title: Text('Carrinho', style: TextStyle(color: Colors.white)),
+            tileColor: Colors.purple,
           ),
+          for (var skin in cart.skins)
+            ListTile(
+              title:
+                  Text(skin.name, style: const TextStyle(color: Colors.white)),
+              subtitle: Text(
+                'Preço: ${NumberFormat.simpleCurrency(locale: 'pt_BR').format(skin.price)}',
+                style: const TextStyle(color: Colors.white),
+              ),
+              trailing: IconButton(
+                icon: const Icon(Icons.remove, color: Colors.white),
+                onPressed: () {
+                  setState(() {
+                    cart.remove(skin);
+                  });
+                },
+              ),
+            ),
+          const Divider(color: Colors.white),
           ListTile(
-            title: const Text('Item 2'),
-            textColor: Colors.white,
-            onTap: () {
-              print('Item 2 clicado');
-            },
+            title: const Text('Total', style: TextStyle(color: Colors.white)),
+            subtitle: Text(
+              NumberFormat.simpleCurrency(locale: 'pt_BR')
+                  .format(cart.getTotalPrice()),
+              style: const TextStyle(color: Colors.white),
+            ),
           ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              onPressed: () {
+                // Adicione a lógica de compra aqui, como checkout, etc.
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                      content: Text('Compra realizada com sucesso!')),
+                );
+              },
+              child: const Text(
+                'Comprar',
+                style: TextStyle(
+                  color: Colors.white, // Cor do texto
+                  fontSize: 18, // Tamanho do texto
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
