20e40bfa818bf86ea566a9cf8f2e41d6bcc57a12
Matheus Baraldi <matheushbaraldi@gmail.com>
2024-11-06 18:11:43 -0300

    feat: implemented menu

--- csgo_skins_app/lib/components/menu.dart
+++ csgo_skins_app/lib/components/menu.dart
@@ -1,4 +1,9 @@
+import 'package:csgo_skins_app/main.dart';
 import 'package:flutter/material.dart';
+import 'package:csgo_skins_app/services/auth_service.dart';
+import 'package:csgo_skins_app/services/user_service.dart';
+import 'package:csgo_skins_app/domain/entities/user.dart';
+import 'package:http/http.dart' as http;
 
 class Menu extends StatelessWidget {
   const Menu({super.key});
@@ -10,28 +15,161 @@ class Menu extends StatelessWidget {
       child: ListView(
         children: [
           ListTile(
-            title: const Text('Home'),
-            textColor: Colors.white,
+            title: const Text('Home', style: TextStyle(color: Colors.white)),
             onTap: () {
-              print('Item 2 clicado');
+              Navigator.pushReplacementNamed(context, '/home');
             },
           ),
           ListTile(
-            textColor: Colors.white,
-            title: const Text('Entrar'),
+            title: const Text('Entrar', style: TextStyle(color: Colors.white)),
             onTap: () {
-              print('Item 1 clicado');
+              _showLoginModal(context);
             },
           ),
           ListTile(
-            textColor: Colors.white,
-            title: const Text('Registrar-se'),
+            title: const Text('Registrar-se',
+                style: TextStyle(color: Colors.white)),
             onTap: () {
-              print('Item 2 clicado');
+              _showRegisterModal(context);
             },
           ),
         ],
       ),
+    );
+  }
+
+  void _showLoginModal(BuildContext context) {
+    final authService = AuthService(http.Client());
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        String email = '';
+        String password = '';
+        return AlertDialog(
+          title: const Text('Entrar'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                decoration: const InputDecoration(labelText: 'Email'),
+                onChanged: (value) {
+                  email = value;
+                },
+              ),
+              TextField(
+                obscureText: true,
+                decoration: const InputDecoration(labelText: 'Senha'),
+                onChanged: (value) {
+                  password = value;
+                },
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancelar'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                try {
+                  final user =
+                      await authService.login(email: email, pass: password);
+                  Navigator.of(context).pop();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Bem-vindo, ${user.name}!')),
+                  );
+
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => HomePage(user: user),
+                    ),
+                  );
+                } catch (e) {
+                  Navigator.of(context).pop();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Falha ao fazer login')),
+                  );
+                }
+              },
+              child: const Text('Entrar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // Função para mostrar o modal de registro
+  void _showRegisterModal(BuildContext context) {
+    final userService = UserService(http.Client());
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        String name = '';
+        String email = '';
+        String password = '';
+        return AlertDialog(
+          title: const Text('Registrar-se'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                decoration: const InputDecoration(labelText: 'Nome'),
+                onChanged: (value) {
+                  name = value;
+                },
+              ),
+              TextField(
+                decoration: const InputDecoration(labelText: 'Email'),
+                onChanged: (value) {
+                  email = value;
+                },
+              ),
+              TextField(
+                obscureText: true,
+                decoration: const InputDecoration(labelText: 'Senha'),
+                onChanged: (value) {
+                  password = value;
+                },
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancelar'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                try {
+                  final newUser =
+                      User(name: name, email: email, password: password);
+                  await userService.create(newUser);
+                  Navigator.of(context).pop();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                        content: Text('Cadastro realizado com sucesso!')),
+                  );
+                } catch (e) {
+                  Navigator.of(context).pop();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Falha ao registrar')),
+                  );
+                }
+              },
+              child: const Text('Registrar'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
