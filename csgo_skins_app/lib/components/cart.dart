f31ca4f032e7fe30ef68176094bc20a786af1851
Matheus <matheushbaraldi@gmail.com>
2024-11-05 21:44:10 -0300

    feat: add white color to text

--- csgo_skins_app/lib/components/cart.dart
+++ csgo_skins_app/lib/components/cart.dart
@@ -6,16 +6,19 @@ class Cart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
+      backgroundColor: const Color.fromARGB(255, 20, 20, 20),
       child: ListView(
         children: [
           ListTile(
             title: const Text('Item 1'),
+            textColor: Colors.white,
             onTap: () {
               print('Item 1 clicado');
             },
           ),
           ListTile(
             title: const Text('Item 2'),
+            textColor: Colors.white,
             onTap: () {
               print('Item 2 clicado');
             },
