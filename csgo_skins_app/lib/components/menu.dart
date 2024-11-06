c2b919ce469ed12ea8aee636e3f58270e2c83e69
Matheus <matheushbaraldi@gmail.com>
2024-11-05 21:44:18 -0300

    feat: add colors

--- csgo_skins_app/lib/components/menu.dart
+++ csgo_skins_app/lib/components/menu.dart
@@ -6,21 +6,25 @@ class Menu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
+      backgroundColor: const Color.fromARGB(255, 20, 20, 20),
       child: ListView(
         children: [
           ListTile(
             title: const Text('Home'),
+            textColor: Colors.white,
             onTap: () {
               print('Item 2 clicado');
             },
           ),
           ListTile(
+            textColor: Colors.white,
             title: const Text('Entrar'),
             onTap: () {
               print('Item 1 clicado');
             },
           ),
           ListTile(
+            textColor: Colors.white,
             title: const Text('Registrar-se'),
             onTap: () {
               print('Item 2 clicado');
